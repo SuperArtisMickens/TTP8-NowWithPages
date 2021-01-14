9a9c64fc5104d4dbc143adbcb9fe71ffd950dd1c
SuperArtisMickens <kdkartismickens@gmail.com>
2021-01-14 18:28:07 -0500

    Changes to Radio Button (Radio Bug)

--- src/components/Poll.js
+++ src/components/Poll.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { withStyles } from '@material-ui/core/styles';
+import { green } from '@material-ui/core/colors';
 import Radio from '@material-ui/core/Radio';
 
 
@@ -8,52 +10,59 @@ class Poll extends React.Component {
 
 	//other member functions and variables can go here
 
+	handlechange(event) {
+		console.log(event.target);
+		event.target.checked=true;
+		//event.target.color=green;
+	  };
+
 	render() {
 		//JS code goes here
 		// but not all of your code
 		//const classes = useStyles();
 		//const  = (event) => event.();
 
-
-
-
-
-
-
 		return (
 			//JSX HTML goes here
 			<>
-
-
 				<p>Who is your FAVORITE Bank President?</p>
-
-
 				<div>
-					<Radio value="a"
+					<Radio 
+						checked={false}
+						color={green[600]}
+						value="Yao Ho"
 						name="radio-button-demo"
-						onChange=""
+						onChange={this.handlechange}
 						inputProps={{ 'aria-label': 'A' }}
 					/> Yau Ho
-      				<Radio value="b"
+					  <Radio 
+						checked={false}  
+						color={green[600]}
+						value="Henji"
 						name="radio-button-demo"
-						onChange=""
+						onChange={this.handlechange}
 						inputProps={{ 'aria-label': 'B' }}
 					/>Henji
-      				<Radio value="d"
+					  <Radio 
+						checked={false}  
+						color={green[600]}
+						value="Deon"
 						name="radio-button-demo"
-						onChange=""
+						onChange={this.handlechange}
 						inputProps={{ 'aria-label': 'D' }}
 					/>Deon
-      				<Radio value="e"
+					  <Radio 
+						checked={false}  
+						color={green[600]}
+						value="Kris"
 						name="radio-button-demo"
-						onChange=""
+						onChange={this.handlechange}
 						inputProps={{ 'aria-label': 'E' }}
 					/> Kris
     			</div>
 		</>
 		)
 	}
-
 }
 
 export default Poll
